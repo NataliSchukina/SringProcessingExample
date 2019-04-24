713c30bd526739f6b18ed02c5ce92e95cbec753c
NataliSchukina <nata_schukina@bk.ru>
2019-04-24 16:40:01 +0300

    Initial commit

---
+++ src/com/epam/nataliya_shchukina/lesson1/task2/ApplicationProcessing.java
@@ -0,0 +1,8 @@
+package com.epam.nataliya_shchukina.lesson1.task2;
+
+
+
+public class ApplicationProcessing {
+
+
+}
---
+++ src/com/epam/nataliya_shchukina/lesson1/task2/StringApplicationRunner.java
@@ -0,0 +1,10 @@
+package com.epam.nataliya_shchukina.lesson1.task2;
+
+public class StringApplicationRunner {
+    public static void main(String[] args) {
+
+        Application app = new Application();
+
+        app.runApplication();
+    }
+}
