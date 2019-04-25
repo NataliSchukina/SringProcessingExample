347cc21932f8be79c27a440cbd476b1fb5b62f70
NataliSchukina <nata_schukina@bk.ru>
2019-04-25 15:53:03 +0300

    version 25/04/2019 a.m.

--- src/com/epam/nataliya_shchukina/lesson1/task2/Application.java
+++ src/com/epam/nataliya_shchukina/lesson1/task2/Application.java
@@ -3,29 +3,12 @@ package com.epam.nataliya_shchukina.lesson1.task2;
 import java.util.Scanner;
 
 public class Application {
-    public String[] stringArrayInit() {
-        int n;
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите количество строк: ");
-
-        if (scanner.hasNextInt()) {
-            n = scanner.nextInt();
-        }else {
-            System.out.print("Введены некорректные данные!");
-            return;
-        }
-
-        String[] strings = new String[n];
-
-        for (int i = 0; i < n; i++) {
-            System.out.print("Введите строку номер " + (i + 1) + ": ");
-            strings[i] = scanner.next();
-        }  return strings;
-    }
     public void runApplication() {
 
         ApplicationProcessing strArrayProcessing = new ApplicationProcessing();
-        var strarray = stringArrayInit();
+        var strarray = strArrayProcessing.stringArrayInit();
+       strArrayProcessing.GetMinString(strarray);
+       strArrayProcessing.GetMaxString(strarray);
     }
 }
--- src/com/epam/nataliya_shchukina/lesson1/task2/ApplicationProcessing.java
+++ src/com/epam/nataliya_shchukina/lesson1/task2/ApplicationProcessing.java
@@ -1,8 +1,55 @@
 package com.epam.nataliya_shchukina.lesson1.task2;
 
 
+import java.util.Scanner;
 
 public class ApplicationProcessing {
+    public String[] stringArrayInit() {
+        int n;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Введите количество строк: ");
+        n = scanner.nextInt();
+       /* if (scanner.hasNextInt()) {
+            n = scanner.nextInt();
+        } else {
+            System.out.print("Введены некорректные данные!");
+            return ;
+        }*/
 
+        String[] strings = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Введите строку номер " + (i + 1) + ": ");
+            strings[i] = scanner.next();
+        }
+        return strings;
+    }
+    public void GetMinString ( String [] strarray) {
+        int strinlengthgmin = strarray[0].length() ;
+        String strminlength = strarray[0];
+              for (String value : strarray) {
+
+            if (value.length() < strinlengthgmin) {
+                strinlengthgmin = value.length();
+                strminlength = value;
+            }
+        }
+        System.out.println("minlength is: "+ strinlengthgmin);
+        System.out.println("Sring with minlength is: "+ strminlength );
+    }
+    public void GetMaxString ( String [] strarray) {
+        int strinlengthgmax = strarray[0].length() ;
+        String strmaxlength = strarray[0];
+        for (String value : strarray) {
+
+            if (value.length() >  strinlengthgmax) {
+                strinlengthgmax = value.length();
+                strmaxlength = value;
+            }
+        }
+        System.out.println("maxlength is: "+ strinlengthgmax);
+        System.out.println("Sring with maxlength is: "+ strmaxlength );
+    }
 }
+
